bc03a699712c83998ccdeec1679aa1f8e9460f9e
Mehmet Şirin Usanmaz <musanmaz@snapbytes.com>
2019-03-19 15:22:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('tiny'))
+
+app.get('/gs', (req, res) => {
+    res.header("Content-Type",'application/json');
+    res.json({ 
+            message: 'Mehmet Şirin Usanmaz',
+            champion : 'Şampiyon Galatasaray'
+        })
+})
+
+app.listen('1905')
